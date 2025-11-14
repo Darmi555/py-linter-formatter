6a7fea786c836cbec5392ca5c32ef23ad70c4a0c
Mateusz <mateusz.rudnicki05@wp.pl>
2025-11-14 22:24:47 +0100

    Done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,44 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            {
+                "line": point["line_number"],
+                "column": point["column_number"],
+                "message": point["text"],
+                "name": point["code"],
+                "source": "flake8"
+            } for point in errors
+        ],
+        "path": file_path,
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": [
+                {
+                    "line": point["line_number"],
+                    "column": point["column_number"],
+                    "message": point["text"],
+                    "name": point["code"],
+                    "source": "flake8"
+                }
+                for point in linter_report[src]
+            ] if linter_report[src] else [],
+            "path": src,
+            "status": "passed" if len(linter_report[src]) == 0 else "failed"
+        }
+        for src in linter_report
+    ]
